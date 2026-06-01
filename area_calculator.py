da49dc0eb44cf7e16aa7949480bd3a838b7d5cc6
analysisbysohan <buddyy2206@gmail.com>
2026-06-01 19:09:43 +0530

    area_calculator.py

---
+++ area_calculator.py
@@ -0,0 +1,13 @@
+# area_calculator.py
+
+def circle(r):
+    return 3.14 * r * r
+
+def square(s):
+    return s * s
+
+def rectangle(l, b):
+    return l * b
+
+def triangle(b, h):
+    return 0.5 * b * h
