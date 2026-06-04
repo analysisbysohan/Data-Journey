5660e1d39e0c13dbed5570895992612beb3a1931
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-06-04 19:48:44 +0530

    Update area_calculator.py
    basic

--- area_calculator.py
+++ area_calculator.py
@@ -1,13 +1,17 @@
 # area_calculator.py
 
 def circle(r):
+    """Calculates the area of a circle given its radius."""
     return 3.14 * r * r
 
 def square(s):
+    """Calculates the area of a square given its side length."""
     return s * s
 
 def rectangle(l, b):
+    """Calculates the area of a rectangle given its length and breadth."""
     return l * b
 
 def triangle(b, h):
+    """Calculates the area of a triangle given its base and height."""
     return 0.5 * b * h
