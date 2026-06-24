b017fbd87d649429778fd9adb5d9ef48254551dd
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-06-24 23:24:11 +0530

    Update conditionals.py

--- PYTHON/conditionals.py
+++ PYTHON/conditionals.py
@@ -1,1 +1,8 @@
+# Program to find the maximum of two numbers entered by the user
+a = int(input("Enter the value for 'a': "))
+b = int(input("Enter the value for 'b': "))
 
+if a > b:
+    print(f"The larger number is: {a}")
+else:
+    print(f"The larger number is: {b}")
