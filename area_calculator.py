24b7c01f16d599b7c0b0a2c372710798cba9f1ec
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-04 19:20:19 +0530

    Update area_calculator.py

--- area_calculator.py
+++ area_calculator.py
@@ -21,7 +21,7 @@ def sphere(r)
 
 def Cylinder(r,h)
         """Calculates the area of a Cylinder given its radius."""
-    return 2 * 3.14 * r * h + 2 * 3.14 * r * h
+    return  2 * 3.14 * r* (r + h)
 
 # 2 * 3.14 * r* (r + h)
 
