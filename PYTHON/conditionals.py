c2e72f7a197ae3a420fe352bd5cf8be3e4c98f0f
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-06 19:18:59 +0530

    Update conditionals.py

--- PYTHON/conditionals.py
+++ PYTHON/conditionals.py
@@ -3,7 +3,7 @@ a = int(input("Enter the value for 'a': "))
 b = int(input("Enter the value for 'b': "))
 
 if a > b:
-    print(f"The larger number is: {a}")
+    print(f"The larger number is: {a}") 
 else:
     print(f"The larger number is: {b}")
       
